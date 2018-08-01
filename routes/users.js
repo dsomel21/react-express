7344e3c178ee9f6b0baed92403f40e1ebbbd1e50
Dilraj Grobo <dilraj@grobo.io>
2018-07-31 21:59:40 -0400

    run PORT=3001 npm start to run node epxress server

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {
+      id: 1,
+      username: "Yeezy"
+    },
+    {
+      id: 2,
+      username: "Grobosexual"
+    }
+  ]);
 });
 
 module.exports = router;
